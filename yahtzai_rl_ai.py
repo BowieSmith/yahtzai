4093cddfe8598ae31231b02d2ec59d59ef6de377
Bowie Smith <r.bowie.smith@gmail.com>
2018-11-09 12:13:16 -0500

    First draft RL AI

---
+++ yahtzai_rl_ai.py
@@ -0,0 +1,33 @@
+import yahtzai_core as yc
+import itertools
+
+def ai_engine(player, rnd, turnNumber, dice):
+    playedMovesBitString = player.playedMovesAsInt()
+    playedMovesAsInt = int('0b' + playedMovesBitString, 2)
+
+    diceStateAsString = dice.asString()
+
+def initializeActionTable(numberPossibleMoves=6):
+    actionDict = {}
+    for playedMoves in range(0, 2 ** numberPossibleMoves):
+        for diceCombination in itertools.combinations_with_replacement("123456", 5):
+            for action in range(0, 2**5):
+                actionDict[(playedMoves, ''.join(diceCombination), action)] = (0,0,0)
+    return actionDict
+
+def evaluateAction(diceString, action):
+    dice = yc.Dice(diceString)
+    actionString = bin(action)[2:].zfill(5)
+    diceToReroll = []
+    for idx,val in enumerate(list(actionString)):
+        if (val == '1'):
+            diceToReroll.append(idx + 1)
+    dice.roll(*diceToReroll)
+    return dice.asString()
+
+def scoreAction(playedMovesBitString, prevDiceString, action, nextDiceString):
+
+def scoreFunction(diceString, scoreEnum):
+    if (scoreEnum.value < 6):
+        return yc.getScoreGivenDice(scoreEnum, yc.Dice(diceString)) + (1.666666 * scoreEnum.value)
+
